1487805eafb48ba207bd99e3f6e85f62e5c306c0
ayoub212-source <71142393+ayoub212-source@users.noreply.github.com>
2025-02-17 17:05:06 +0100

    Create index.php

---
+++ MARKETPLACE/PAGES/index.php
@@ -0,0 +1,30 @@
+<?php include('header.php')?>
+<?php include('navbar.php')?>
+
+    
+
+    <div class="container-fluid banner">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="Slogan">
+                    <p>Vendigo là où chaque produit rencontre son acheteur, sans tracas ni limites.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+
+    </div>
+
+
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasRightLabel">Mon panier</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <!-- le contenue du panier -->
+    </div>
+    </div>
+<?php include('footer.php')?>
